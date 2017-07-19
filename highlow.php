a2183314e08e84304a91c40d8bb50df3a9d7ba71
Vivian Canales <viviancans@gmail.com>
2017-07-19 12:13:53 -0500

    added is numericto check if guess is a number

--- highlow.php
+++ highlow.php
@@ -1,45 +1,32 @@
 <?php 
 
-// fwrite(STDOUT, 'How old are you? ');
+	$compNumber = mt_rand(1, 100);
+	$guesses = 1;
 
-// $age = trim(fgets(STDIN));
+	echo $compNumber . PHP_EOL;
 
+	do {
 
-// fwrite(STDOUT, "Wow, you are $age years old\n");
+		fwrite(STDOUT, 'Guess? ');
 
+		$userInput = trim(fgets(STDIN));
 
-// if ($age > 25){
-// 	echo "You are no spring chicken" . PHP_EOL;
-// } else {
-// 	echo "You are a baby!" . PHP_EOL;
-// }
+		if (!is_numeric($userInput) ){
+			echo "Please enter a valid number." . PHP_EOL;
 
+		} elseif ($userInput < $compNumber){
+			echo "Higher" . PHP_EOL;
+			$guesses++;
 
+		} elseif ($userInput > $compNumber) {
+			echo "Lower" . PHP_EOL;
+			$guesses++;
 
-$compNumber = rand(1, 100);
-$guesses = 0;
-
-echo $compNumber . PHP_EOL;
-
-do {
-
-	fwrite(STDOUT, 'Guess ');
-
-	$userInput = trim(fgets(STDIN));
-
-	if ($userInput < $compNumber){
-		echo "Higher" . PHP_EOL;
-		$guesses++;
-
-	} elseif ($userInput > $compNumber) {
-		echo "Lower" . PHP_EOL;
-		$guesses++;
-
-	} else {
-		echo "Good Guess! You Win!" . PHP_EOL;
-		echo "Number of guesses: {$guesses}"  . PHP_EOL;
-	}
-} while ($userInput != $compNumber);
+		} else {
+			echo "Good Guess! You Win!" . PHP_EOL;
+			echo "Number of guesses: {$guesses}"  . PHP_EOL;
+		}
+	} while ($userInput != $compNumber);
 
 
 
