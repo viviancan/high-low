9afcf59cfc09f53bc45360b13853255fe7a445a7
Vivian Canales <viviancans@gmail.com>
2017-07-20 09:23:33 -0500

    added php arguments to change the min/max values of game

--- highlow.php
+++ highlow.php
@@ -1,6 +1,46 @@
 <?php 
 
-	$compNumber = mt_rand(1, 100);
+	// $compNumber = mt_rand(1, 100);
+	// $guesses = 0;
+	
+
+	//   echo $compNumber . PHP_EOL;
+
+	// do {
+
+	// 	fwrite(STDOUT, 'Guess? ');
+
+	// 	$guesses++;
+
+	// 	$userInput = trim(fgets(STDIN));
+
+	// 	if (!is_numeric($userInput) ){
+	// 		echo "Please enter a valid number." . PHP_EOL;
+
+	// 	} elseif ($guesses == 7) {
+	// 		echo "Game over! Too many guesses!" . PHP_EOL;
+	// 		break;
+
+	// 	}  elseif ($userInput < $compNumber){
+	// 		echo "Higher" . PHP_EOL;
+	// 		// $guesses++;
+
+	// 	} elseif ($userInput > $compNumber) {
+	// 		echo "Lower" . PHP_EOL;
+
+	// 	} else {
+	// 		echo "Good Guess! You Win!" . PHP_EOL;
+	// 		echo "Number of guesses: {$guesses}"  . PHP_EOL;
+	// 	}
+	// } while ($guesses <= 7 && $userInput != $compNumber  );
+
+
+
+
+
+
+
+	$compNumber = mt_rand($argv[1], $argv[2]);
 	$guesses = 0;
 	
 
@@ -33,7 +73,6 @@
 			echo "Number of guesses: {$guesses}"  . PHP_EOL;
 		}
 	} while ($guesses <= 7 && $userInput != $compNumber  );
-
 
 
 ?>
